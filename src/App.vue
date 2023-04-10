8cddf5d0435cb8476aeeff1e90319d3d35ad041a
JonathanAleksei <j.suarez.a.garcia@gmail.com>
2023-04-10 15:12:54 -0500

    frontend v1

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="container">
+    <h1 id="title">Torre - User Skills </h1><br>
+    <CustomInput v-model="username"/><br>
+    <button @click="fetchUser">Show Skills</button><br>
+    <h2 id="name">{{ user.name }}</h2><br>
+    <img v-bind:src="user.picture" alt=""><br>
+    <ul>
+      <li v-for="(skill, index) in user.skills" :key="index">{{ skill.name }}</li>
+    </ul>
+  </div>
+  
+</template>
+
+<script>
+import axios from "axios";
+import CustomInput from './components/CustomInput.vue';
+
+export default {
+  components: { CustomInput },
+   data(){
+    return {
+      user: {
+        name: '',
+        picture: '',
+        skills: []
+      },
+      username : 'getuser' 
+    }
+   },
+
+//Get user Axios
+   methods: {
+    fetchUser () {
+      console.log(this.username)
+      axios
+        .get(`http://localhost:3000/users/${this.username}`)
+        .then((response) => {
+          console.log(response.data)
+          this.user.name = response.data.person.name
+          this.user.skills = response.data.strengths
+          this.user.picture = response.data.person.picture
+          console.log(this.user.name)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
+<style scoped>
+ #title{
+  color: aliceblue;
+  text-align: center;
+ }
+
+ #container{
+  padding: 40px 80px;
+  border: rgb(139, 141, 141) 2px solid;
+  border-radius: 10px;
+  background-color: #27292d;
+  margin-top: 10%;
+ }
+
+ #name{
+  color: aliceblue;
+  text-align: center;
+  margin-top: 10%;
+ }
+</style>
---
+++ src/components/CustomInput.vue
@@ -0,0 +1,23 @@
+<template>
+    <input v-bind="$attrs" v-model="internalValue" @input="updateValue" placeholder="Search using UserName e.g. torrenegra01"/>
+  </template>
+  
+  <script>
+  export default {
+    name: 'CustomInput',
+    props: {
+      value: [String, Number],
+    },
+    data() {
+      return {
+        internalValue: this.value,
+      };
+    },
+    methods: {
+      updateValue(event) {
+        this.internalValue = event.target.value;
+        this.$emit('input', this.internalValue);
+      },
+    },
+  };
+  </script>
