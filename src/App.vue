bd25974ccaa7511483d3d813574ca6f4226cb07a
JonathanAleksei <j.suarez.a.garcia@gmail.com>
2023-04-10 21:49:56 -0500

    frontend v1.1

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="container">
-    <h1 id="title">Torre - User Skills </h1><br>
-    <CustomInput v-model="username"/><br>
-    <button @click="fetchUser">Show Skills</button><br>
-    <h2 id="name">{{ user.name }}</h2><br>
-    <img v-bind:src="user.picture" alt=""><br>
-    <ul>
-      <li v-for="(skill, index) in user.skills" :key="index">{{ skill.name }}</li>
-    </ul>
+    <h1 id="title">Torre - User Skills </h1>
+    <CustomInput v-model="username" />
+    <button @click="fetchUser" id="buttonskills">Show Skills</button>
+    <img id="profileimg" v-bind:src="user.picture" alt="">
+    <h2 id="name">{{ user.name }}</h2>
+    <div>
+      <ul>
+        <li v-for="(skill, index) in user.skills" :key="index" id="list">{{ skill.name }}</li>
+      </ul>
+    </div>
   </div>
-  
 </template>
 
 <script>
@@ -18,29 +19,27 @@ import CustomInput from './components/CustomInput.vue';
 
 export default {
   components: { CustomInput },
-   data(){
+  data() {
     return {
       user: {
         name: '',
         picture: '',
-        skills: []
+        skills: [],
+        proficiency: ''
       },
-      username : 'getuser' 
+      username: 'getuser'
     }
-   },
+  },
 
-//Get user Axios
-   methods: {
-    fetchUser () {
-      console.log(this.username)
+  //Get user Axios
+  methods: {
+    fetchUser() {
       axios
-        .get(`https://torre-api-henna.vercel.app/users/${this.username}`) 
+        .get(`https://torre-api-henna.vercel.app/users/${this.username}`)
         .then((response) => {
-          console.log(response.data)
           this.user.name = response.data.person.name
           this.user.skills = response.data.strengths
           this.user.picture = response.data.person.picture
-          console.log(this.user.name)
         })
         .catch((error) => {
           console.log(error)
@@ -50,23 +49,4 @@ export default {
 }
 </script>
 
-<style scoped>
- #title{
-  color: aliceblue;
-  text-align: center;
- }
-
- #container{
-  padding: 40px 80px;
-  border: rgb(139, 141, 141) 2px solid;
-  border-radius: 10px;
-  background-color: #27292d;
-  margin-top: 10%;
- }
-
- #name{
-  color: aliceblue;
-  text-align: center;
-  margin-top: 10%;
- }
-</style>
+<style scoped></style>
--- src/components/CustomInput.vue
+++ src/components/CustomInput.vue
@@ -1,23 +1,34 @@
 <template>
-    <input v-bind="$attrs" v-model="internalValue" @input="updateValue" placeholder="Search using UserName e.g. torrenegra01"/>
-  </template>
+  <input v-bind="$attrs" v-model="internalValue" @input="updateValue"
+    placeholder="Search using UserName e.g. torrenegra01" />
+</template>
   
-  <script>
-  export default {
-    name: 'CustomInput',
-    props: {
-      value: [String, Number],
+<script>
+export default {
+  name: 'CustomInput',
+  props: {
+    value: [String, Number],
+  },
+  data() {
+    return {
+      internalValue: this.value,
+    };
+  },
+  methods: {
+    updateValue(event) {
+      this.internalValue = event.target.value;
+      this.$emit('input', this.internalValue);
     },
-    data() {
-      return {
-        internalValue: this.value,
-      };
-    },
-    methods: {
-      updateValue(event) {
-        this.internalValue = event.target.value;
-        this.$emit('input', this.internalValue);
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
+
+<style>
+input {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  height: 50px;
+}
+</style>
